c21a51329846292d7b85a3f4734cfa35272e19de
doobee2 <seulbee0224@gmail.com>
2023-07-18 18:29:18 +0900

    library and Inner Class, Exception1

---
+++ ch07/src/sec2/Example1.java
@@ -0,0 +1,11 @@
+package sec2;
+    //ch05.jar import
+import sec1.Board;
+import sec1.Member;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Member m1 = new Member();
+
+    }
+}
